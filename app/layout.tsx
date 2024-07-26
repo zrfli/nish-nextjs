064fdedb8611facb773db6af8cd51c12b33ff96c
Eray <78818724+zrfli@users.noreply.github.com>
2024-07-26 14:11:23 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,11 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
-
-const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
-
 import "@/styles/globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: 'Nişantaşı Üniversitesi',
