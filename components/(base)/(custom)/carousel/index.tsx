c65d6e02311e3153524f98e3386db880d93e850c
Eray <78818724+zrfli@users.noreply.github.com>
2024-08-04 22:06:27 +0300

    Update index.tsx

--- components/(base)/(custom)/carousel/index.tsx
+++ components/(base)/(custom)/carousel/index.tsx
@@ -14,7 +14,7 @@ const BaseCarousel = () => {
       <Swiper navigation={true} modules={[Navigation]}>
         {Array.from({ length: 9 }).map((_, index) => (
           <SwiperSlide className="relative min-h-96 h-full w-full" key={index}>
-            <Image src="/0ce3cfb8-dcca-47f1-afd7-db00dab10be0.jpg" fill alt="" priority={true} style={{ objectFit: "cover" }} />
+            <Image src="/0190f88b-c3f4-7275-b503-99359feea678.webp" fill alt="" priority={true} style={{ objectFit: "cover" }} />
           </SwiperSlide>
         ))}
       </Swiper>
